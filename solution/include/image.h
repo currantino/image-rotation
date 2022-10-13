f787f69209d2de1223f6840ad979bbe1d7c376be
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-13 15:03:47 +0300

    feat: Add image.h, image.c

--- solution/include/image.h
+++ solution/include/image.h
@@ -1,19 +1,18 @@
 #pragma once
 
+#include "pixel.h"
 #include <stddef.h>
 #include <stdlib.h>
-#include <inttypes.h>
 
 #include "dimensions.h"
 
-struct pixel {
-  uint8_t components[3];
-};
+#define BYTES_PER_PIXEL 3
 
 struct image {
-  struct dimensions size;
-  struct pixel* data;
+	struct dimensions size;
+	struct pixel *data;
 };
 
-struct image image_create( struct dimensions size );
-void image_destroy( struct image* image );
+struct image image_create(struct dimensions size);
+size_t image_get_padding(struct image *image);
+void image_destroy(struct image *image);
