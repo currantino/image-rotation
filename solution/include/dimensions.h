87b411eaf7b00ca1be223b4600a8c367673ede1b
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-13 15:04:19 +0300

    feat: Add dimensions.h, dimensions.c

--- solution/include/dimensions.h
+++ solution/include/dimensions.h
@@ -3,8 +3,8 @@
 #include <stddef.h>
 
 struct dimensions {
-  size_t x;
-  size_t y;
+	size_t x;
+	size_t y;
 };
 
-struct dimensions dimensions_reverse( const struct dimensions* dim );
+struct dimensions dimensions_reverse(const struct dimensions *dim);
--- solution/src/dimensions.c
+++ solution/src/dimensions.c
@@ -1,6 +1,7 @@
 #include "dimensions.h"
 
-struct dimensions dimensions_reverse( const struct dimensions* dim) {
-	struct dimensions reversed_dim = {.x = dim -> y, .y = dim -> x};
-	return reversed_dim;	
+struct dimensions dimensions_reverse(const struct dimensions *dim)
+{
+	struct dimensions reversed_dim = {.x = dim->y, .y = dim->x};
+	return reversed_dim;
 }
