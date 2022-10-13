649100c29a31774011e058bfee65782bdfbf1acf
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-13 15:03:47 +0300

    feat: Add image.h, image.c

--- solution/src/image.c
+++ solution/src/image.c
@@ -1,12 +1,28 @@
 #include "image.h"
 
-struct image image_create( struct dimensions size ) {
-	struct pixel* data = malloc(sizeof(struct pixel) * size.x * size.y);
+struct image image_create(struct dimensions size)
+{
+	struct pixel *data = malloc(sizeof(struct pixel) * size.x * size.y);
 	struct image created_image = {.size = size, .data = data};
 	return created_image;
 }
 
-void image_destroy( struct image* image ) {
-	free(image -> data);
+size_t image_get_padding(struct image *image)
+{
+	size_t width_in_bytes = ((image->size).x) * BYTES_PER_PIXEL;
+	size_t padding_in_bytes = 0;
+	if (width_in_bytes % 4 == 0) {
+		return padding_in_bytes;
+	} else {
+		while ((width_in_bytes + padding_in_bytes) % 4 != 0) {
+			padding_in_bytes++;
+		}
+		return padding_in_bytes;
+	}
+}
+
+void image_destroy(struct image *image)
+{
+	free(image->data);
 	free(image);
 }
