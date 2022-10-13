f6de32b5b11a8dfa674ff97f8f7030fc75c0a4cc
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-13 15:05:03 +0300

    feat: Read and write struct bmp_header

--- solution/include/bmp_header.h
+++ solution/include/bmp_header.h
@@ -1,30 +1,29 @@
 #ifndef BMP_HEADER_H
 #define BMP_HEADER_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-struct __attribute__((packed)) bmp_header
-{
-        uint16_t bfType;
-        uint32_t  bfileSize;
-        uint32_t bfReserved;
-        uint32_t bOffBits;
-        uint32_t biSize;
-        uint32_t biWidth;
-        uint32_t  biHeight;
-        uint16_t  biPlanes;
-        uint16_t biBitCount;
-        uint32_t biCompression;
-        uint32_t biSizeImage;
-        uint32_t biXPelsPerMeter;
-        uint32_t biYPelsPerMeter;
-        uint32_t biClrUsed;
-        uint32_t  biClrImportant;
+struct __attribute__((packed)) bmp_header {
+	uint16_t bfType;
+	uint32_t bfileSize;
+	uint32_t bfReserved;
+	uint32_t bOffBits;
+	uint32_t biSize;
+	uint32_t biWidth;
+	uint32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	uint32_t biXPelsPerMeter;
+	uint32_t biYPelsPerMeter;
+	uint32_t biClrUsed;
+	uint32_t biClrImportant;
 };
 
-bool read_header_from_file( const char* filename, struct bmp_header* header );
+bool read_header_from_file(FILE *f, struct bmp_header *header);
+bool write_header_to_file(FILE *f, struct bmp_header *header);
 
 #endif
-
