7edc0973e76d5b5d6326abb00c66f429095ad454
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-18 13:57:15 +0300

    fix: Change parameter names in image.h

--- solution/include/image.h
+++ solution/include/image.h
@@ -14,6 +14,6 @@ struct image {
 struct image image_create(struct dimensions size);
 size_t image_get_padding_in_bytes(const struct image *image);
 void image_destroy(struct image *image);
-size_t image_get_width(const struct image *image);
-size_t image_get_height(const struct image *image);
-size_t image_get_size(const struct image *image);
+size_t image_get_width(const struct image *img);
+size_t image_get_height(const struct image *img);
+size_t image_get_size(const struct image *img);
