5896e9f8d88d9e2d5e9eae795b78cac7db0f6433
Matvey Smorodin <mat.smooooder@gmail.com>
2022-10-18 16:33:40 +0300

    refactor: Move BYTES_PER_PIXEL to bmp_io.c from image.c

--- solution/src/image.c
+++ solution/src/image.c
@@ -11,12 +11,6 @@ struct image image_create(const struct dimensions size)
 	return created_image;
 }
 
-size_t image_get_padding_in_bytes(const struct image *image)
-{
-	size_t width_in_bytes = (image->size.x) * BYTES_PER_PIXEL;
-	return width_in_bytes % 4 == 0 ? 0 : 4 - width_in_bytes % 4;
-}
-
 void image_destroy(struct image *image)
 {
 	free(image->data);
